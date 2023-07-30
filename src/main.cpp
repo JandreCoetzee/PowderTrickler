66b69ac2987ed4d0ea07722ed7acc4da13204351
Jandre' Coetzee <coetzee.jandre@gmail.com>
2023-07-30 17:49:27 +0200

    Basic spin of stepper motor

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,23 @@
 #define TFT_RST 50
 
 #define EN_PIN           38 // Enable
-#define DIR_PIN          55 // Direction
-#define STEP_PIN         54 // Step
+#define DIR_PIN          12 // Direction
+#define STEP_PIN         14 // Step
 #define CS_PIN           42 // Chip select
 #define SW_MOSI          66 // Software Master Out Slave In (MOSI)
 #define SW_MISO          44 // Software Master In Slave Out (MISO)
 #define SW_SCK           64 // Software Slave Clock (SCK)
 #define SW_RX            63 // TMC2208/TMC2224 SoftwareSerial receive pin
 #define SW_TX            40 // TMC2208/TMC2224 SoftwareSerial transmit pin
-#define SERIAL_PORT Serial1 // TMC2208/TMC2224 HardwareSerial port
+#define SERIAL_PORT Serial2 // TMC2208/TMC2224 HardwareSerial port
 #define R_SENSE 0.11f // Match to your driver
                      // SilentStepStick series use 0.11
                      // UltiMachine Einsy and Archim2 boards use 0.2
                      // Panucatt BSD2660 uses 0.1
                      // Watterott TMC5160 uses 0.075
 
-//TMC2208Stepper driver = TMC2208Stepper(&SERIAL_PORT, R_SENSE); // Hardware Serial0
-TMC2208Stepper driver = TMC2208Stepper(SW_RX, SW_TX, R_SENSE); // Software serial
+TMC2208Stepper driver = TMC2208Stepper(&SERIAL_PORT, R_SENSE); // Hardware Serial0
+//TMC2208Stepper driver = TMC2208Stepper(SW_RX, SW_TX, R_SENSE); // Software serial
 constexpr uint32_t steps_per_mm = 80;
 AccelStepper stepper = AccelStepper(stepper.DRIVER, STEP_PIN, DIR_PIN);
 
@@ -63,7 +63,7 @@ void btnCallback(DFRobot_UI::sButton_t &btn,DFRobot_UI::sTextBox_t &obj) {
 }
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin();
   //Initialize UI 
   ui.begin();
   //Set the UI theme, there are two themes to choose from: CLASSIC and MODERN.
@@ -102,32 +102,50 @@ void setup() {
   btn3.setOutput(&tb);
   ui.draw(&btn3,/**x=*/(screen.width()/10)*7,/**y=*/screen.height()/2,/*width*/screen.width()/10*2,/*height*/screen.width()/10*2);
 
+SERIAL_PORT.begin(9600);
+   // SPI.begin();
+   //    pinMode(CS_PIN, OUTPUT);
+   //  digitalWrite(CS_PIN, HIGH);
+   //  driver.begin();             // Initiate pins and registeries
+   //  driver.rms_current(600);    // Set stepper current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5); Set motor RMS current
+   //  //driver.en_pwm_mode(1);      // Enable extremely quiet stepping, Toggle stealthChop on TMC2130/2160/5130/5160
+   //  driver.pwm_autoscale(1);    // Needed for stealthChop
+   //  //driver.en_spreadCycle(false); // Toggle spreadCycle on TMC2208/2209/2224
+   //  driver.microsteps(16);       // Set microsteps to 1/nth
 
-   SPI.begin();
-  pinMode(CS_PIN, OUTPUT);
-    digitalWrite(CS_PIN, HIGH);
-    driver.begin();             // Initiate pins and registeries
-    driver.rms_current(600);    // Set stepper current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5); Set motor RMS current
-    //driver.en_pwm_mode(1);      // Enable extremely quiet stepping, Toggle stealthChop on TMC2130/2160/5130/5160
-    driver.pwm_autoscale(1);    // Needed for stealthChop
-    //driver.en_spreadCycle(false); // Toggle spreadCycle on TMC2208/2209/2224
-    driver.microsteps(16);       // Set microsteps to 1/nth
+   //  stepper.setMaxSpeed(50*steps_per_mm); // 100mm/s @ 80 steps/mm
+   //  stepper.setAcceleration(1000*steps_per_mm); // 2000mm/s^2
+   //  stepper.setEnablePin(EN_PIN);
+   //  stepper.setPinsInverted(false, false, true);
+   //  stepper.enableOutputs();
 
-    stepper.setMaxSpeed(50*steps_per_mm); // 100mm/s @ 80 steps/mm
-    stepper.setAcceleration(1000*steps_per_mm); // 2000mm/s^2
-    stepper.setEnablePin(EN_PIN);
-    stepper.setPinsInverted(false, false, true);
-    stepper.enableOutputs();
+  pinMode(EN_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  SPI.begin(); 
+  SERIAL_PORT.begin(115200);
+   driver.begin();                 //  SPI: Init CS pins and possible SW SPI pins
+                                  // UART: Init SW UART (if selected) with default 115200 baudrate
+  driver.toff(5);                 // Enables driver in software
+  driver.rms_current(600);        // Set motor RMS current
+  driver.microsteps(16);          // Set microsteps to 1/16th
+
+//driver.en_pwm_mode(true);       // Toggle stealthChop on TMC2130/2160/5130/5160
+//driver.en_spreadCycle(false);   // Toggle spreadCycle on TMC2208/2209/2224
+  driver.pwm_autoscale(true);     // Needed for stealthChop
 }
+
+bool shaft = false;
 
 void loop() {
   ui.refresh();
 
-   if (stepper.distanceToGo() == 0) {
-        stepper.disableOutputs();
-        delay(100);
-        stepper.move(100*steps_per_mm); // Move 100mm
-        stepper.enableOutputs();
-    }
-    stepper.run();
+   for (uint16_t i = 50000; i > 0; i--) {
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(160);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(160);
+  }
+  shaft = !shaft;
+  driver.shaft(shaft);
 }
